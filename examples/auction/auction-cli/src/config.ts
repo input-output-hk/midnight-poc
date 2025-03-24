4431d2f24580c6a550dcccf72ee353117275cd70
mineme0110 <shailesh.patil@iohk.io>
2025-03-24 12:16:49 +0000

    update the testnet config
    Signed-off-by: mineme0110 <shailesh.patil@iohk.io>

--- examples/auction/auction-cli/src/config.ts
+++ examples/auction/auction-cli/src/config.ts
@@ -46,13 +46,28 @@ export class StandaloneConfig implements Config {
   }
 }
 
-export class TestnetRemoteConfig implements Config {
+export class TestnetRemoteConfig1 implements Config {
   privateStateStoreName = 'auction-private-state';
   logDir = path.resolve(currentDir, '..', 'logs', 'testnet-remote', `${new Date().toISOString()}.log`);
   zkConfigPath = path.resolve(currentDir, '..', '..', 'contract', 'dist', 'managed', 'auction');
   indexer = 'https://indexer.testnet.midnight.network/api/v1/graphql';
   indexerWS = 'wss://indexer.testnet.midnight.network/api/v1/graphql/ws';
   node = 'https://rpc.testnet.midnight.network';
+  proofServer = 'http://127.0.0.1:6300';
+  issuerBaseUrl = 'http://192.168.1.86:8085';
+  mediatorBaseUrl = 'http://192.168.1.86:8080';
+  setNetworkId() {
+    setNetworkId(NetworkId.TestNet);
+  }
+}
+
+export class TestnetRemoteConfig implements Config {
+  privateStateStoreName = 'auction-private-state';
+  logDir = path.resolve(currentDir, '..', 'logs', 'testnet-remote', `${new Date().toISOString()}.log`);
+  zkConfigPath = path.resolve(currentDir, '..', '..', 'contract', 'dist', 'managed', 'auction');
+  indexer = 'https://indexer.testnet-02.midnight.network/api/v1/graphql';
+  indexerWS = 'wss://indexer.testnet-02.midnight.network/api/v1/graphql/ws';
+  node = 'https://rpc.testnet-02.midnight.network';
   proofServer = 'http://127.0.0.1:6300';
   issuerBaseUrl = 'http://192.168.1.86:8085';
   mediatorBaseUrl = 'http://192.168.1.86:8080';
